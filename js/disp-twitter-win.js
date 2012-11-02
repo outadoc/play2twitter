6bc2ba564e6b80c538377a504d1ab34e5f22f3cd
outadoc <outadoc@gmail.com>
2012-11-02 17:23:17 +0100

    Reduced the size of the tweet window

--- js/disp-twitter-win.js
+++ js/disp-twitter-win.js
@@ -1,5 +1,5 @@
 if(document.getElementById("playerSongTitle") == null) {
 	alert("Hmm... there's nothing to share at the moment.\nStart playing a song, and then click the icon!");
 } else {
-	window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent("#NowPlaying " + document.getElementById("playerSongTitle").childNodes[0].textContent + ' by ' + document.getElementById("playerArtist").childNodes[0].textContent), null, "width=900,height=600");
+	window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent("#NowPlaying " + document.getElementById("playerSongTitle").childNodes[0].textContent + ' by ' + document.getElementById("playerArtist").childNodes[0].textContent), null, "width=400,height=250");
 }
